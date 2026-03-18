908bf27ae075454693f1de03d8868f1cd95ed9ff
ThanhRoo <thanhvo130602@gmail.com>
2026-03-18 15:43:00 +0700

    API Address Profile

--- src/address/dto/create-address.dto.ts
+++ src/address/dto/create-address.dto.ts
@@ -1,1 +1,32 @@
-export class CreateAddressDto {}
+import { IsString, IsBoolean, IsOptional, IsNotEmpty, Length } from 'class-validator';
+
+export class CreateAddressDto {
+  @IsString()
+  fullName: string;
+
+  @IsString()
+  @IsNotEmpty({ message: 'Phone is required' })
+  @Length(10,10,{message: 'Phone must be 10 digits'})
+  phone: string;
+
+  @IsString()
+  @IsNotEmpty({ message: 'Province is required' })
+  province: string;
+
+  @IsString()
+  @IsNotEmpty({ message: 'District is required' })
+  district: string;
+
+  @IsString()
+  @IsNotEmpty({ message: 'Ward is required' })
+  ward: string;
+
+  @IsString()
+  @IsNotEmpty({ message: 'Detail address is required' })
+  detailAddress: string;
+
+  @IsOptional()
+  @IsBoolean()
+  isDefault?: boolean;
+
+}
