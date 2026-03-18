79178f7d47e36f4b912ec1e5f38b947db23d1f85
ThanhRoo <thanhvo130602@gmail.com>
2026-03-18 15:43:00 +0700

    API Address Profile

--- src/address/address.controller.ts
+++ src/address/address.controller.ts
@@ -1,34 +1,42 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Request, UseGuards } from '@nestjs/common';
 import { AddressService } from './address.service';
 import { CreateAddressDto } from './dto/create-address.dto';
 import { UpdateAddressDto } from './dto/update-address.dto';
+import { JwtAuthGuard } from 'src/auth/passport/jwt-auth.guard';
 
 @Controller('address')
 export class AddressController {
   constructor(private readonly addressService: AddressService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post()
-  create(@Body() createAddressDto: CreateAddressDto) {
-    return this.addressService.create(createAddressDto);
+  create(
+    @Request() req,
+    @Body() createAddressDto: CreateAddressDto) {
+    return this.addressService.createAddress(req.user.userId,createAddressDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get()
-  findAll() {
-    return this.addressService.findAll();
+  findAll(@Request() req) {
+    return this.addressService.findAll(req.user.userId);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.addressService.findOne(+id);
+  findOne(@Request() req, @Param('id') id: string) {
+    return this.addressService.findOne(req.user.userId, +id);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAddressDto: UpdateAddressDto) {
-    return this.addressService.update(+id, updateAddressDto);
+  update(@Request() req, @Param('id') id: string, @Body() updateAddressDto: UpdateAddressDto) {
+    return this.addressService.updateAddress(req.user.userId, +id, updateAddressDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.addressService.remove(+id);
+  remove(@Request() req, @Param('id') id: string) {
+    return this.addressService.remove(req.user.userId, +id);
   }
 }
--- src/address/dto/update-address.dto.ts
+++ src/address/dto/update-address.dto.ts
@@ -1,4 +1,32 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateAddressDto } from './create-address.dto';
+import { IsString, IsBoolean, IsOptional, IsNotEmpty, Length } from 'class-validator';
 
-export class UpdateAddressDto extends PartialType(CreateAddressDto) {}
+export class UpdateAddressDto {
+  @IsString()
+  fullName: string;
+
+  @IsString()
+  @IsNotEmpty({ message: 'Phone is required' })
+  @Length(10,10,{message: 'Phone must be 10 digits'})
+  phone: string;
+
+  @IsString()
+  @IsNotEmpty({ message: 'Province is required' })
+  province: string;
+
+  @IsString()
+  @IsNotEmpty({ message: 'District is required' })
+  district: string;
+
+  @IsString()
+  @IsNotEmpty({ message: 'Ward is required' })
+  ward: string;
+
+  @IsString()
+  @IsNotEmpty({ message: 'Detail address is required' })
+  detailAddress: string;
+
+  @IsOptional()
+  @IsBoolean()
+  isDefault?: boolean;
+
+}
